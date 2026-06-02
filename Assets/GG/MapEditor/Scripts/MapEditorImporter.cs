13f890286f1d8fee3641bb5e61ea09e74f3cd97a
Duck <54730815+mimorgan@users.noreply.github.com>
2026-06-02 16:47:56 +0900

    Add bbmapproject support 📽️

--- Assets/GG/MapEditor/Scripts/MapEditorImporter.cs
+++ Assets/GG/MapEditor/Scripts/MapEditorImporter.cs
@@ -2,12 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
+using System.Linq;
 using System.Threading.Tasks;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace GG.BeanBattles.MapEditor
 {
-    public static class MapEditorLoader
+    public static class MapEditorImporter
     {
         public static List<string> GetAllEditorMapsPaths()
         {
@@ -61,5 +66,46 @@ namespace GG.BeanBattles.MapEditor
 
             return extractPath;
         }
+
+#if UNITY_EDITOR
+
+        public static bool ImportProject(string folderPath)
+        {
+            // the import location is just default assets, so the paths/refs should match
+            string importPath = Path.Combine(Application.dataPath);
+            if (!Directory.Exists(importPath)) Directory.CreateDirectory(importPath);
+
+            string assetsPath = Path.Combine(folderPath, "Assets");
+
+            // copy all files / assets / metas > techincally they could have no unique assets
+            if (Directory.Exists(assetsPath)) CopyDirectory(assetsPath, importPath);
+
+           // the main scene should be in the first folder, remember its path
+            string scenePath = Directory.GetFiles(folderPath, "*.unity", SearchOption.TopDirectoryOnly).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(scenePath))
+            { Debug.LogWarning("Imported files, but couldn't find scene"); return true; }
+
+            string importatedScenePath = Path.Combine(importPath, Path.GetFileName(scenePath));
+            File.Copy(scenePath, importatedScenePath, true);
+
+            string sceneMeta = scenePath + ".meta";
+            if (File.Exists(sceneMeta)) File.Copy(sceneMeta, importatedScenePath + ".meta", true);
+   
+            // refresh and open
+            AssetDatabase.Refresh();
+            UnityEditor.SceneManagement.EditorSceneManager.OpenScene(importatedScenePath);
+
+            return true;
+        }
+
+        private static void CopyDirectory(string sourceDir, string targetDir)
+        {
+            if (!Directory.Exists(targetDir)) Directory.CreateDirectory(targetDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir)) File.Copy(file, Path.Combine(targetDir, Path.GetFileName(file)), true);
+            foreach (var directory in Directory.GetDirectories(sourceDir)) CopyDirectory(directory, Path.Combine(targetDir, Path.GetFileName(directory)));
+        }
+#endif
     }
 }
